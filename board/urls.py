8cd27b653a8fb8a10dc13a83a2fe4b303496a8c6
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-24 12:25:01 +0900

    .

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path ,include
 from .views import *
 urlpatterns = [    
-    path('', board_lists, name='board_lists'),
+    path('new/', board_new, name='board_new'), # create
+    path('', board_lists, name='board_lists'),  # read
+    path('update/<int:pk>', board_update, name='board_update'), # update
+    path('delete/<int:pk>', board_delete, name='board_delete'), # delete
+    
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
 # Create your views here.
 def board_lists(request):
     return render(request,'board/lists.html')
+
+def board_new(request):
+    if request.method=='POST':
+        Post.objects.create(title = request.POST['title'],
+                            content = request.POST['content'])
+        return redirect('board_lists')
+    return render(request,'board/new.html')
+
+def board_update(request,id):    
+    post = get_object_or_404(Post,id=id)
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.save()
+        return redirect('board_lists')
+    return render(request,'board/update.html', {'post':post})
+
+def board_delete(request,id):
+    post = get_object_or_404(Post,id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('board_lists')
+    return render(request,'board/delete.html', {'post':post})
